a4385ee8ab66b9fa83dc29db0eea475a59d83ad5
Tom Heard <tcheard@gmail.com>
2017-11-05 20:43:38 +1300

    Add basic CLI

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 .PHONY: build clean dep
+GIT_SHA=`git rev-parse --short HEAD`
 
 build:
 	@echo "Building blockchain..."
 	@mkdir -p bin
-	@go build -o bin/blockchain .
+	@go build -ldflags "-X main.GitSHA=${GIT_SHA}" -o bin/blockchain .
 
 clean:
 	@rm -f bin/*
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 func addBlock(bc *Blockchain, data string) {
@@ -16,34 +14,6 @@ func addBlock(bc *Blockchain, data string) {
 }
 
 func main() {
-	bc, err := NewBlockchain()
-	if err != nil {
-		log.Fatal("Failed to get blockchain", err)
-		os.Exit(1)
-	}
-	defer bc.db.Close()
-
-	addBlock(bc, "Send 1 BTC to Nikkii")
-	addBlock(bc, "Send another 2 BTC to Nikkii")
-
-	iter := bc.Iterator()
-
-	for {
-		block, err := iter.Next()
-		if err != nil {
-			log.Fatal("Failed to retrieve next block", err)
-			os.Exit(1)
-		}
-
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
+	cli := CLI{}
+	cli.Run()
 }
