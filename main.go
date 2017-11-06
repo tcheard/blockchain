914f304db77bff0185429786ee723d3b53176ea4
Tom Heard <tcheard@gmail.com>
2017-11-06 19:08:23 +1300

    Remove unused function

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"log"
-	"os"
-)
-
-func addBlock(bc *Blockchain, data string) {
-	err := bc.AddBlock(data)
-	if err != nil {
-		log.Fatal("Failed to add block")
-		os.Exit(1)
-	}
-}
-
 func main() {
 	cli := CLI{}
 	cli.Run()
