84bb1cfb656842cffc3d41d14578b19d73adab14
Tom Heard <tcheard@gmail.com>
2017-11-06 19:10:31 +1300

    Add transaction types

---
+++ transaction.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
+
+const subsidy = 10
+
+// Transaction represents a blockchain transaction
+type Transaction struct {
+	ID   []byte
+	Vin  []TXInput
+	Vout []TXOutput
+}
+
+// IsCoinbase checks whether the transaction is a coinbase transaction
+func (tx Transaction) IsCoinbase() bool {
+	return len(tx.Vin) == 1 && len(tx.Vin[0].Txid) == 0 && tx.Vin[0].Vout == -1
+}
+
+// SetID sets the ID of a transaction
+func (tx Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
+}
+
+// TXInput represents a transaction input
+type TXInput struct {
+	Txid      []byte
+	Vout      int
+	ScriptSig string
+}
+
+// TXOutput represents a transaction output
+type TXOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+// NewCoinbaseTX creates a new coinbase transaction
+func NewCoinbaseTX(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+
+	txin := TXInput{
+		Txid:      []byte{},
+		Vout:      -1,
+		ScriptSig: data,
+	}
+	txout := TXOutput{
+		Value:        subsidy,
+		ScriptPubKey: to,
+	}
+
+	tx := Transaction{
+		ID:   nil,
+		Vin:  []TXInput{txin},
+		Vout: []TXOutput{txout},
+	}
+
+	tx.SetID()
+
+	return &tx
+}
